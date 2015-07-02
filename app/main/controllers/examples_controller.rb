f25b9af113b67a2ffcbb5e6ae60b50eec31de77e
George D. Plymale II <georgedp@orbitalimpact.com>
2015-07-02 18:24:38 -0400

    added working animation examples

--- app/main/controllers/examples_controller.rb
+++ app/main/controllers/examples_controller.rb
@@ -5,6 +5,17 @@ require 'examples/lib/basics/load_an_image'
 require 'examples/lib/basics/move_an_image'
 require 'examples/lib/basics/render_text'
 require 'examples/lib/basics/tween_an_image'
+
+require 'examples/lib/animation/animation_events'
+require 'examples/lib/animation/change_frame'
+require 'examples/lib/animation/change_texture_on_click'
+require 'examples/lib/animation/destroy_animation'
+require 'examples/lib/animation/dynamic_animation'
+require 'examples/lib/animation/group_creation'
+require 'examples/lib/animation/load_texture'
+require 'examples/lib/animation/looped_animation'
+require 'examples/lib/animation/multiple_anims'
+
 require 'pp'
 
 module Main
@@ -23,20 +34,42 @@ module Main
     end
 
     def template_ready
-      if params._example == "click_on_an_image"
-        $example = ClickOnAnImage::Game.new
-      elsif params._example == "image_follow_input"
-        $example = ImageFollowInput::Game.new
-      elsif params._example == "load_an_animation"
-        $example = LoadAnAnimation::Game.new
-      elsif params._example == "load_an_image"
-        $example = LoadAnImage::Game.new
-      elsif params._example == "move_an_image"
-        $example = MoveAnImage::Game.new
-      elsif params._example == "render_text"
-        $example = RenderText::Game.new
-      elsif params._example == "tween_an_image"
-        $example = TweenAnImage::Game.new
+      if params._category == "basics"
+        if params._example == "click_on_an_image"
+          $example = ClickOnAnImage::Game.new
+        elsif params._example == "image_follow_input"
+          $example = ImageFollowInput::Game.new
+        elsif params._example == "load_an_animation"
+          $example = LoadAnAnimation::Game.new
+        elsif params._example == "load_an_image"
+          $example = LoadAnImage::Game.new
+        elsif params._example == "move_an_image"
+          $example = MoveAnImage::Game.new
+        elsif params._example == "render_text"
+          $example = RenderText::Game.new
+        elsif params._example == "tween_an_image"
+          $example = TweenAnImage::Game.new
+        end
+      elsif params._category == "animation"
+        if params._example == "animation_events"
+          $example = AnimationEvents::Game.new
+        elsif params._example == "change_frame"
+          $example = ChangeFrame::Game.new
+        elsif params._example == "change_texture_on_click"
+          $example = ChangeTextureOnClick::Game.new
+        elsif params._example == "destroy_animation"
+          $example = DestroyAnimation::Game.new
+        elsif params._example == "dynamic_animation"
+          $example = DynamicAnimation::Game.new
+        elsif params._example == "group_creation"
+          $example = GroupCreation::Game.new
+        elsif params._example == "load_texture"
+          $example = LoadTexture::Game.new
+        elsif params._example == "looped_animation"
+          $example = LoopedAnimation::Game.new
+        elsif params._example == "multiple_anims"
+          $example = MultipleAnims::Game.new
+        end
       end
     end
   end
