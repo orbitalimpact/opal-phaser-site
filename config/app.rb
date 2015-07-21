d27ba457759ac123f5aa9c253eef50e0e39d58d6
George D. Plymale II <georgedp@orbitalimpact.com>
2015-07-21 17:04:57 -0400

    fix typo

--- config/app.rb
+++ config/app.rb
@@ -3,8 +3,8 @@ Volt.configure do |config|
   config.db_driver = 'mongo'
   config.db_name = (config.app_name + '_' + Volt.env.to_s)
 
-  if ENV['MONGOHQ_URL'].present?
-    config.db_uri = ENV['MONGOHQ_URL'] # you will have to set this on heroku
+  if ENV['MONGOLAB_URL'].present?
+    config.db_uri = ENV['MONGOLAB_URL'] # you will have to set this on heroku
   else
     config.db_host = 'localhost'
     config.db_port = 27017
