e6668448713d293ee51db284a6076604ea31f492
George D. Plymale II <georgedp@orbitalimpact.com>
2015-09-04 14:56:47 -0400

    we don't need user templates

--- app/main/config/dependencies.rb
+++ app/main/config/dependencies.rb
@@ -8,10 +8,10 @@ component 'bootstrap'
 component 'bootstrap_jumbotron_theme'
 
 # provides templates for login, signup, and logout
-component 'user_templates'
+#component 'user_templates'
 
 component 'examples'
 
 component 'code_highlight'
 
-css_file "/assets/code_highlight/assets/css/sunburst.css"
+css_file '/app/code_highlight/assets/css/sunburst.css'
--- app/main/config/routes.rb
+++ app/main/config/routes.rb
@@ -4,10 +4,6 @@ client '/', controller: 'main'
 client '/examples', controller: 'examples', action: 'index'
 client '/examples/{{ category }}/{{ example }}', controller: 'examples', action: 'example'
 
-# Routes for login and signup, provided by user_templates component gem
-#client '/signup', component: 'user_templates', controller: 'signup'
-#client '/login', component: 'user_templates', controller: 'login'
-
 # The main route, this should be last. It will match any params not
 # previously matched.
 client '/', {}
